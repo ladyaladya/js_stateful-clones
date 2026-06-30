f6a1668ab73560817280e35ad60941ad0fa4901d
Vladyslav Vyshnivskyi <vladyslav.vyshnivskyi@avanquest.com>
2026-06-30 14:16:23 +0300

    Update func

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -30,6 +30,9 @@ function transformStateWithClones(state, actions) {
       case 'clear':
         currentState = {};
         break;
+
+      default:
+        throw new Error(`Unknown action type: ${action.type}`);
     }
 
     states.push(currentState);
